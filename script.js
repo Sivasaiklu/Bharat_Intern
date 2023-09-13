3a392dc9ebf3756756597e9b36200f891ac6c3bc
sivasai <sivasainookala@gmail.com>
2023-09-13 16:25:14 +0530

    bharat_intern_task2_html_file

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const c1 = document.getElementById("Celsius");
+const f1 = document.getElementById("Fahrenheit");
+const k1 = document.getElementById("Kelvin");
+
+
+function tempConvert(){
+    const currentValue = +event.target.value;
+    switch(event.target.name){
+        case "Celsius":
+            f1.value = (currentValue * 1.8) + 32;
+            k1.value = currentValue + 273.15;
+            break;
+        case "Fahrenheit":
+            c1.value = (currentValue - 32) / 1.8;
+            k1.value = (currentValue - 32) /1.8 + 273.15;
+            break;    
+        case "Kelvin":
+            c1.value = currentValue - 273.15;
+            f1.value = (currentValue - 273.15) * 1.8 + 32;
+            break;     
+
+        default:
+            break;    
+    }
+}
